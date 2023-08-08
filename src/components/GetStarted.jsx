8918053fa8cf6c9b0ef41dc277e7614ab34b001e
Fateme Ghalandari <fghalandarii@gmail.com>
2023-08-08 20:00:32 +0330

    added GetStarted component

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,8 +1,18 @@
 import { arrowUp } from "../assets";
 const GetStarted = () => (
-    <div className="flex justify-center items-center w-[140px] h-[140px] rounded-full bg-blue-gradient ">
-
-</div>
-)
+  <div className="flex justify-center items-center w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px]">
+    <div className="h-full w-full bg-primary rounded-full flex flex-col justify-center items-center">
+      <div className="flex flex-row justify-center items-start">
+        <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+          <span className="text-gradient">Get</span>
+        </p>
+        <img src={arrowUp} alt="arrow" className="h-[23px] w-[23px]" />
+      </div>
+      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+        <span className="text-gradient">Started</span>
+      </p>
+    </div>
+  </div>
+);
 
 export default GetStarted;
