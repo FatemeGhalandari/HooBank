6d24832b72ad1b6892158516d09789d9a2cf5594
Fateme Ghalandari <fghalandarii@gmail.com>
2023-08-09 16:39:10 +0330

    finished Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { discount } from "../assets";
+import { discount, robot } from "../assets";
 import GetStarted from "./GetStarted";
 const Hero = () => (
   <section className="flex md:flex-row flex-col sm:py-16 py-6">
@@ -13,13 +13,35 @@ const Hero = () => (
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 text-white font-poppins font-semibold text-[52px] ss:text-[72px] leading-[75px] ss:leading-[100px]">
           The Next <br className=" sm:block hidden" />{" "}
-          <span className="text-gradient">Generation</span> Payment Method.
+          <span className="text-gradient">Generation</span>
         </h1>
 
         <div className="ss:flex hidden md:mr-4 mr-0">
           <GetStarted />
         </div>
       </div>
+      <h1 className=" text-white font-poppins font-semibold text-[52px] ss:text-[72px] leading-[75px] ss:leading-[100px]">
+        Payment Method.{" "}
+      </h1>
+      <p className="font-poppins font-normal text-dimWhite text-[18px] max-w-[470px] mt-5">
+        Our team of experts uses a methodology to identify the credit cards most
+        likely to fit your needs. We examine annual percentage rates, annual
+        fees.
+      </p>
+    </div>
+    <div className="flex flex-1 relative my-10 md:my-0 items-center justify-center ">
+      <img
+        src={robot}
+        alt="robot"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+      //gradients under the robot image
+      <div className="absolute z-[0] w-[50%] h-[50%] top-0 pink__gradient" />
+      <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+    </div>
+    <div className="ss:hidden flex items-center justify-center">
+      <GetStarted />
     </div>
   </section>
 );
