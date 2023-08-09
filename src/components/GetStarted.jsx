3e5a2315aaeb481d7b969c30d42bf93e131ee665
Fateme Ghalandari <fghalandarii@gmail.com>
2023-08-09 20:32:35 +0330

    fixed bugs

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,12 +1,12 @@
 import { arrowUp } from "../assets";
 const GetStarted = () => (
-  <div className="flex justify-center items-center w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px]">
-    <div className="h-full w-full bg-primary rounded-full flex flex-col justify-center items-center">
+  <div className="flex justify-center items-center w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer">
+    <div className="h-[100%] w-[100%] bg-primary rounded-full flex flex-col justify-center items-center">
       <div className="flex flex-row justify-center items-start">
         <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
           <span className="text-gradient">Get</span>
         </p>
-        <img src={arrowUp} alt="arrow" className="h-[23px] w-[23px]" />
+        <img src={arrowUp} alt="arrow" className="h-[23px] w-[23px] object-contain" />
       </div>
       <p className="font-poppins font-medium text-[18px] leading-[23px]">
         <span className="text-gradient">Started</span>
