36b33210a9d9bc8fecf254621ec77122db2ad812
Fateme Ghalandari <fghalandarii@gmail.com>
2023-08-16 21:57:59 +0330

    Finished Clients component

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-
-const Clients = () => {
-  return (
-    <div>
-      
+import React from "react";
+import { clients } from "../constants";
+const Clients = () => (
+  <section className="flex justify-center items-center my-4">
+    <div className="flex justify-center items-center  flex-wrap w-full">
+      {clients.map((client) => (
+        <div key={client.id}  className="flex flex-1 justify-center items-center min-w-[120px] sm:min-w-[192px]">
+          <img src={client.logo} alt="client" className="w-[100px] sm:w-[192px] object-contain"/>
+        </div>
+      ))}
     </div>
-  )
-}
-
-export default Clients
+  </section>
+);
+export default Clients;
