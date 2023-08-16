6089e93972719931415e1cd27159931622c86a2f
Fateme Ghalandari <fghalandarii@gmail.com>
2023-08-16 22:13:31 +0330

    Finished CTA component

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import Button from "./Button";
 const CTA = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="flex flex-col sm:flex-row sm:px-16 px-6 sm:py-12 py-4 sm:my-16 my-6 bg-black-gradient-2 rounded-[20px] box-shadow">
+      <div className="flex flex-col flex-1 ">
+        <h2 className="font-poppins font-semibold text-[40px] sm:text-[48px] text-white">
+          Let’s try our service now!
+        </h2>
+        <p className="font-poppins font-normal text-[20px]  text-dimWhite max-w-[470px] mt-5">
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+      <div className="flex justify-center items-center ml-0 mt-10 sm:ml-10 sm:mt-0">
+        <Button />
+      </div>
+    </section>
+  );
+};
 
-export default CTA
+export default CTA;
