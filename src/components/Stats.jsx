5e58e4d733477c0fa05b503c3c7bd6f99cced63c
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-29 17:41:46 -0400

    chore: Update Stats component to improve font sizes and line heights

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,9 +3,16 @@ import { stats } from "../constants";
 const Stats = () => (
   <section className="flex flex-row flex-wrap justify-center items-center mb-6 sm:mb-20">
     {stats.map((stat) => (
-      <div key={ stat.id} className="flex-1 flex flex-row justify-start items-center">
-        <h4 className="font-poppins font-semibold text-white text-[30px] xs:text-[40px]">{stat.value}</h4>
-        <p className=" font-poppins font-normal text-gradient text-[15px] xs:text-[20px] uppercase ml-3">{stat.title}</p>
+      <div
+        key={stat.id}
+        className="flex-1 flex flex-row justify-start items-center"
+      >
+        <h4 className="font-poppins font-semibold text-white text-[40px] xs:text-[48px] xs:leading-[76.8px] leading-[66.8px]">
+          {stat.value}
+        </h4>
+        <p className=" font-poppins font-normal text-gradient text-[18px] leading-[30px] uppercase ml-3">
+          {stat.title}
+        </p>
       </div>
     ))}
   </section>
