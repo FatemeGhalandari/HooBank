dba0091d3c0716d375935d19e35a1817ff4f79b2
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-29 17:39:15 -0400

    chore: Refactor App component to use section tags for better semantic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Navbar,
   Billing,
@@ -11,6 +10,7 @@ import {
   Stats,
   Testimonials,
 } from "./components";
+
 const App = () => (
   <div className="w-full bg-primary overflow-hidden">
     <div className="px-6 sm:px-16 flex justify-center items-center">
@@ -18,16 +18,35 @@ const App = () => (
         <Navbar />
       </div>
     </div>
-    <div className="bg-primary flex justify-center items-start">
+    <div className="bg-primary flex justify-center items-start" id="Home">
       <div className="w-full xl:max-w-[1280px]">
         <Hero />
       </div>
     </div>
     <div className="flex bg-primary justify-center items-start px-6 sm:px-16 ">
       <div className="w-full xl:max-w-[1280px]">
-        <Stats />
-        <Business /> <Billing /> <CardDeal /> <Testimonials /> <Clients />{" "}
-        <CTA /> <Footer />
+        <section id="Stats">
+          <Stats />
+        </section>
+        <section id="Business">
+          <Business />
+        </section>
+        <section id="Billing">
+          <Billing />
+        </section>
+        <section id="CardDeal">
+          <CardDeal />
+        </section>
+        <section id="Testimonials">
+          <Testimonials />
+        </section>
+        <section id="Clients">
+          <Clients />
+        </section>
+        <section id="CTA">
+          <CTA />
+        </section>{" "}
+        <Footer />
       </div>
     </div>
   </div>
