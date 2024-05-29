d29c388221b9ff4087ef64847328e2d81afb74bb
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-29 17:37:52 -0400

    chore: Update tailwind.config.js with new colors, fonts, and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
+        primary: "#000000",
+        secondary: "#ffffff",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
+        playfair: ["Playfair Display", "serif"],
+        manrope: ["Manrope", "sans-serif"],
       },
+      backgroundImage: {
+        bg1: "url('../src/assets/bg1.png')",
+      },
+      animation: {
+        dropIn: "dropin .5s ease-in-out",
+      },
+
+      // that is actual animation
+      keyframes: () => ({
+        dropin: {
+          "0%": { opacity: 0, transform: "translate(0,-200px)" },
+          "100%": { opacity: 1, transform: "translate(0,0)" },
+        },
+      }),
     },
     screens: {
       xs: "480px",
@@ -23,4 +43,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+});
