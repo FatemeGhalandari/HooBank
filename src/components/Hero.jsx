bc9fa38ea615c9662a7bfb0aceb6f275472b0dc6
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-29 17:41:13 -0400

    chore: Update Hero component to fix layout and add gradients

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,13 +29,13 @@ const Hero = () => (
         fees.
       </p>
     </div>
-    <div className="flex flex-1 relative my-10 md:my-0 items-center justify-center ">
+    <div className="flex flex-1 relative my-10 md:my-0 items-center justify-between ">
       <img
         src={robot}
         alt="robot"
         className="w-[100%] h-[100%] relative z-[5]"
       />
-      //gradients under the robot image
+      {/* gradients under the robot image */}
       <div className="absolute z-[0] w-[50%] h-[50%] top-0 pink__gradient" />
       <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
